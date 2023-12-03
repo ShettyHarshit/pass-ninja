2b5df638f0afae6b5c9144f92745122927a30771
Harshit Shetty <swaharsh@gmail.com>
2023-12-03 13:32:35 +0530

    Add password list component

---
+++ src/components/PasswordList.tsx
@@ -0,0 +1,53 @@
+import { Password } from "@/data/Password";
+import { decryptPassword } from "@/services/encryption";
+import { Button, ListItem, UnorderedList } from "@chakra-ui/react";
+import { useState } from "react";
+
+function PasswordList({ passwords }: { passwords: Password[] }) {
+  return (
+    <div>
+      <center>
+        <h1>Your Passwords</h1>
+      </center>
+      <br />
+      {passwords.map((ps: Password, i: number) => {
+        return <PasswordRow password={ps} key={i} />;
+      })}
+    </div>
+  );
+}
+
+export default PasswordList;
+
+const PasswordRow = ({ password }: { password: Password }) => {
+  const [blurValue, setBlurValue] = useState(true);
+  return (
+    <div>
+      <UnorderedList>
+        <ListItem>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div>
+              <h2>Platform: {password.title}</h2>
+              <h3>
+                Password:{" "}
+                <span className={blurValue ? "blur" : "no-blur"}>
+                  {decryptPassword({ encrypted: password.value })}
+                </span>
+              </h3>
+            </div>
+            <div>
+              <Button
+                onClick={() => {
+                  setBlurValue(!blurValue);
+                }}
+              >
+                Reveal
+              </Button>{" "}
+              <Button>Delete</Button>
+            </div>
+          </div>
+        </ListItem>
+      </UnorderedList>
+    </div>
+  );
+};
