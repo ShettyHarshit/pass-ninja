b667e30545fce95dc45b8dcd869ca7266c5f6968
Harshit Shetty <swaharsh@gmail.com>
2023-12-03 19:06:09 +0530

    Add uuid to password while storing

--- src/services/encryption.ts
+++ src/services/encryption.ts
@@ -2,11 +2,12 @@ import { Password } from "@/data/Password";
 import { SECRET_PASSPHRASE } from "@/data/appConfig";
 // @ts-ignore
 import { AES, enc } from "crypto-js";
+import { v4 as uuidv4 } from "uuid";
 
 export const addPassword = ({ title, value }: Password) => {
   const passwords = JSON.parse(localStorage.getItem("passwords") ?? "[]");
   const encryptedPassword = AES.encrypt(value, SECRET_PASSPHRASE).toString();
-  passwords.push({ title, value: encryptedPassword } as Password);
+  passwords.push({ title, value: encryptedPassword, id: uuidv4() } as Password);
   localStorage.setItem("passwords", JSON.stringify(passwords));
 };
 
@@ -17,7 +18,6 @@ export const fetchPasswords = () => {
 
 export const decryptPassword = ({ encrypted }: { encrypted: string }) => {
   const bytes = AES.decrypt(encrypted, SECRET_PASSPHRASE);
-  console.log(bytes);
   const decrypted = bytes.toString(enc.Utf8);
   return decrypted;
 };
